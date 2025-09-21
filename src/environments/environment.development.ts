5e001f25dc88da9edd5f31f628c8072092ad930b
Koshman-Nikita <63288899+Koshman-Nikita@users.noreply.github.com>
2025-09-21 20:24:05 +0300

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyBmyqcFGk_JFFRIlgH603JOzYb7iLgBpy4',
-    authDomain: 'lesson-booking-fea5b.firebaseapp.com',
-    projectId: 'lesson-booking-fea5b',
-    storageBucket: 'lesson-booking-fea5b.firebasestorage.app',
-    messagingSenderId: '12811495842',
-    appId: '1:12811495842:web:eac5b5a9250ee9e4a1aafb',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 
   timezone: 'Europe/Kyiv',
@@ -13,11 +13,11 @@ export const environment = {
 
   // EmailJS (заміни на свої дані)
   emailjs: {
-    publicKey: 'xmz00v0tQp-JHr0-F',
-    serviceId: 'service_ohctoic',
+    publicKey: '',
+    serviceId: '',
     templates: {
-      received: 'tmpl_received', // шаблон "Заявку отримано"
-      status: 'tmpl_status',     // шаблон "Зміна статусу"
+      received: , // шаблон "Заявку отримано"
+      status: '',     // шаблон "Зміна статусу"
     },
   },
 };
